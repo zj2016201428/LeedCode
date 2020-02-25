d1d0e9268fa6cd334a79f3915bf87d4116f25efe
zj2016201428 <1365015181@qq.com>
2020-02-25 20:45:07 +0800

    new file

---
+++ hello.cpp
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 二  2/25 20:43:12 2020
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<ctime>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<cmath>
+#include<queue>
+using namespace std;
+
+int main() {
+
+    cout << "welcome";
+    return 0;
+}
