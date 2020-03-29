d9f56f8942700c75620fdc1dbb008b342e3ef617
zj2016201428 <1365015181@qq.com>
2020-03-29 13:21:43 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 日  3/29 13:20:49 2020
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<ctime>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<cmath>
+#include<queue>
+using namespace std;
+
+int main() {
+
+    printf("Hello HaiZei!\n");
+    return 0;
+}
